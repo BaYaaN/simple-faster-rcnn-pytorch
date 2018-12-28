511a14e9e16daecff5b2654cd1f8d2bffd63a159
okido <okidoki66@gmail.com>
2018-12-28 12:58:48 +0100

    Demo command

---
+++ demo.py
@@ -0,0 +1,30 @@
+# USE IT INSIDE PYTHON CONSOLE
+
+#
+# %load_ext autoreload
+# %autoreload 2
+# from FasterRcnn.data.util import read_image
+# from FasterRcnn.model import FasterRCNNVGG16
+# from FasterRcnn.utils.vis_tool import vis_bbox
+# from config import opt
+# from FasterRcnn.utils import array_tool as at
+# import torch as t
+# import matplotlib.pyplot as plt
+# %matplotlib tk
+#
+# opt.caffe_pretrain=True
+# img = read_image('misc/demo.jpg')
+# img = t.from_numpy(img)[None]
+# faster_rcnn = FasterRCNNVGG16().cuda()
+#
+#
+# state_dict = t.load('checkpoints/fasterrcnn_12222105_0.712649824453_caffe_pretrain.pth')
+# faster_rcnn.load_state_dict(state_dict['model'])
+#
+# _bboxes, _labels, _scores = faster_rcnn.predict(img,visualize=True)
+# vis_bbox(at.tonumpy(img[0]),
+#          at.tonumpy(_bboxes[0]),
+#          at.tonumpy(_labels[0]).reshape(-1),
+#          at.tonumpy(_scores[0]).reshape(-1))
+
+
