611c6780909cdc7aa065d9be8a52b0d5cbc34e0d
okido <okidoki66@gmail.com>
2018-12-28 11:17:45 +0100

    Remove unimportant stuff

--- misc/convert_caffe_pretrain.py
+++ misc/convert_caffe_pretrain.py
@@ -2,7 +2,6 @@
 # https://github.com/ruotianluo/pytorch-faster-rcnn
 import torch
 from torch.utils.model_zoo import load_url
-from torchvision import models
 
 sd = load_url("https://s3-us-west-2.amazonaws.com/jcjohns-models/vgg16-00b39a1b.pth")
 sd['classifier.0.weight'] = sd['classifier.1.weight']
@@ -16,7 +15,6 @@ del sd['classifier.4.weight']
 del sd['classifier.4.bias']
 
 import  os
-# speicify the path to save
 if not os.path.exists('checkpoints'):
     os.makedirs('checkpoints')
 torch.save(sd, "checkpoints/vgg16_caffe.pth")
